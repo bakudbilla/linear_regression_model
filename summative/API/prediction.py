d963a93d0868a13791f7281697b894cadc1ff9ed
Bernice <berniceawinpang@gmail.com>
2024-11-23 08:06:09 +0000

    ports added

--- summative/API/prediction.py
+++ summative/API/prediction.py
@@ -1,5 +1,7 @@
+import os
 from fastapi import FastAPI
 import numpy as np
+import uvicorn
 from joblib import load
 from pydantic import BaseModel
 
@@ -42,3 +44,6 @@ def predict(data: PredictInput):
         return {"predicted charges": prediction[0]}  
     except Exception as e:
         return {"error": str(e)}
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
