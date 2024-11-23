793e1ebfabb5d70280761abb1e5fb352f9b607cf
Bernice <berniceawinpang@gmail.com>
2024-11-23 08:35:23 +0000

    ports

--- summative/API/prediction.py
+++ summative/API/prediction.py
@@ -44,6 +44,6 @@ def predict(data: PredictInput):
         return {"predicted charges": prediction[0]}  
     except Exception as e:
         return {"error": str(e)}
- if __name__ == "__main__":
+if __name__ == "__main__":
     port = int(os.environ.get("PORT", 10000))
-     uvicorn.run(app, host="0.0.0.0", port=port)
+    uvicorn.run(app, host="0.0.0.0", port=port)
